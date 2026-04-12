58e44e4bd3badea500a06acf20de1be5ac9c16ba
toyo-craft <toyo-craft@example.com>
2026-04-13 01:54:52 +0900

    fix: FileListPanel における kIsWeb の未定義エラーを修正
    - foundation.dart のインポートを追加
    - ブレッドクラム型アドレスバーの実装を安定化

--- lib/ui/panels/file_list_panel.dart
+++ lib/ui/panels/file_list_panel.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:io' as io;
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
